02b9f1c4a42d3b13ccdb3effd14816cb8e21a870
AndrewThian <andrewbobbythian@gmail.com>
2018-08-09 22:10:57 +0800

    update database

--- start_local.sh
+++ start_local.sh
@@ -16,12 +16,19 @@ echo "importing schema from: $DIR/database/schema.sql"
 CONTAINER_ID=budgetbro_dev
 
 docker run --rm -d \
-    --name $CONTAINER_ID \
-    -e POSTGRES_DB=$CONTAINER_ID \
-    -e POSTGRES_USER=$CONTAINER_ID \
-    -e POSTGRES_PASSWORD=docker \
-    -v $VOLUME_DIR:/var/lib/postgresql/data \
-    -v $DIR/database/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
-    -p 5432:5432 \
-    postgres:9.6
+--name $CONTAINER_ID \
+-e POSTGRES_DB=$CONTAINER_ID \
+-e POSTGRES_USER=$CONTAINER_ID \
+-e POSTGRES_PASSWORD=docker \
+-v $VOLUME_DIR:/var/lib/postgresql/data \
+-v $DIR/database/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
+-p 5432:5432 \
+postgres:9.6
     
+if [ `docker inspect -f {{.State.Running}} $CONTAINER_ID` ]; then
+    echo "Postgres started. access via root:root@0.0.0.0:5432/$CONTAINER_ID"
+    echo "Stop the DB with:"
+    echo "docker stop $CONTAINER_ID"
+else
+    echo "could not start container"
+fi
